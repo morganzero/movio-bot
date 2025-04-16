0ef8ea9e495cdacebe810e88a653bdd99353effa
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2025-04-16 09:20:38 +0200

    Update roles.py

--- bot/roles.py
+++ bot/roles.py
@@ -4,13 +4,15 @@ import discord
 with open("product_roles.json", "r") as f:
     PRODUCT_ROLE_MAP = json.load(f)
 
+from db import get_role_by_product
+
 async def assign_roles_from_products(products, member: discord.Member):
     guild = member.guild
     for product in products:
-        name = product.get("name")
-        if name in PRODUCT_ROLE_MAP:
-            role_name = PRODUCT_ROLE_MAP[name]
+        product_name = product.get("name")
+        role_name = get_role_by_product(product_name)
+        if role_name:
             role = discord.utils.get(guild.roles, name=role_name)
             if role and role not in member.roles:
                 await member.add_roles(role)
-                print(f"Assigned role {role_name} to {member.display_name}")
+                print(f"Assigned role '{role.name}' to {member.display_name}")
